37d0a03122dec39294ce7557b6eea98f591cb284
Chris Burns <ckbzxz@gmail.com>
2020-11-12 21:41:10 -0500

    updated matching logic, when an image is clicked it finds pair and calculates price total

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -22,16 +22,58 @@ var listOfFood = ["Steak", "Wings", "Burrito", "Sushi", "Cheese"];
 var listOfDrinks = ["Canadian (Beer)", "Whisky", "Corona", "Sapporo", "Wine"];
 //these are the lists of food and drink so far, we will most likely just use these for testing until we can start pulling from multiple food/drink apis
 
+var foodMenu = {
+    "Burritos" : 6.90,
+    "Pizza" : 12.40,
+    "Wings" : 9.10
+}
+
+var drinkMenu = {
+    "Beer" : 2.70,
+    "Wine" : 12.00,
+    "Cider" : 3.10
+}
+
 //PRIMARY FUNCTIONS -------------------------------------------------------------------------------------------------------------------------------
 
 //this function is called when a user selects an item from the dropdown, the choice is passed in and then this function matches the perfect drink
 function drinkMatch(food) {
-    
+    var foodPrice = foodMenu[food];
+    var matchingDrink;
+    var drinkPrice;
+    if (food === "Burritos") {
+        matchingDrink = "Beer";
+        drinkPrice = drinkMenu[matchingDrink];
+    }
+    if (food === "Pizza") {
+        matchingDrink = "Wine";
+        drinkPrice = drinkMenu[matchingDrink];
+    }
+    if (food === "Wings") {
+        matchingDrink = "Cider";
+        drinkPrice = drinkMenu[matchingDrink];
+    }
+    console.log("Your food choice is " + food + " which pairs perfectly with " + matchingDrink + " and will cost " + Math.round((foodPrice + drinkPrice) * 100) / 100  + "0");
 }
 
 //this is the same as drinkMatch, but does the opposite for when someone selects their drink of choice
 function foodMatch (drink) {
-
+    var drinkPrice = drinkMenu[drink];
+    var matchingFood;
+    var foodPrice;
+    if (drink === "Beer") {
+        matchingFood = "Burritos";
+        foodPrice = foodMenu[matchingFood];
+    }
+    if (drink === "Wine") {
+        matchingFood = "Pizza";
+        foodPrice = foodMenu[matchingFood];
+    }
+    if (drink === "Cider") {
+        matchingFood = "Wings";
+        foodPrice = foodMenu[matchingFood];
+    }
+    console.log("Your drink choice is " + drink + " which pairs perfectly with " + matchingFood + " and will cost " + Math.round((foodPrice + drinkPrice) * 100) / 100  + "0");
 }
 
 
@@ -41,6 +83,7 @@ foodList.addEventListener("click", function (event) {
     var element = event.target;
     if (element.matches("img")) {
         console.log(element.alt);
+        drinkMatch(element.alt);
     }
 })
 
@@ -48,6 +91,7 @@ drinkList.addEventListener("click", function (event) {
     var element = event.target;
     if (element.matches("img")) {
         console.log(element.alt);
+        foodMatch(element.alt);
 
     }
 })
