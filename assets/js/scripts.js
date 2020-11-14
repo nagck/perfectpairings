b192c6bcc04dc54bf0b680526056de220dcab9f7
Chris Burns <ckbzxz@gmail.com>
2020-11-14 12:57:20 -0500

    moved gif fetch call up into control flow

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -75,7 +75,27 @@ function foodMatch (drink) {
     }
     console.log("Your drink choice is " + drink + " which pairs perfectly with " + matchingFood + " and will cost " + Math.round((foodPrice + drinkPrice) * 100) / 100  + "0");
 }
+//FETCH CALLS--------------------------------------------------------------------------------------------------------------------------------------
+function renderGifs(){ 
 
+    var searchTerm = document.querySelector("#search").value
+    //var search = "";
+    
+    //--fetch API key
+    fetch('https://api.giphy.com/v1/gifs/search?q='+""+"&api_key=fhyjxSw2icjRND3sWkVDSIduWRwkEPsI&limit=1")
+    .then(function(response){
+    //--response and return  
+        return response.json()
+    })
+    //-- generate the image for the gif
+    .then(function(response){
+        var gifContainer = document.querySelector("#gifContainer")
+        gifContainer.innerHTML=""
+        var gif = document.createElement('img')
+        gif.setAttribute('src', response.data[0].images.fixed_height.url) 
+        gifContainer.appendChild(gif) 
+    })
+    }
 
 //EVENT LISTENERS -------------------------------------------------------------------------------------------------------------------------------
 
@@ -105,27 +125,3 @@ modalClose.addEventListener("click", function (event) {
         modalMain.classList.remove("is-active");
     }
 } )
-
-
-//Pieter GIF contribution
-
-function renderGifs(){ 
-
-    var searchTerm = document.querySelector("#search").value
-    //var search = "";
-    
-    //--fetch API key
-    fetch('https://api.giphy.com/v1/gifs/search?q='+""+"&api_key=fhyjxSw2icjRND3sWkVDSIduWRwkEPsI&limit=1")
-    .then(function(response){
-    //--response and return  
-        return response.json()
-    })
-    //-- generate the image for the gif
-    .then(function(response){
-        var gifContainer = document.querySelector("#gifContainer")
-        gifContainer.innerHTML=""
-        var gif = document.createElement('img')
-        gif.setAttribute('src', response.data[0].images.fixed_height.url) 
-        gifContainer.appendChild(gif) 
-    })
-    }
