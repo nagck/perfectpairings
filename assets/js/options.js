fcd66f92712b7587433309080f184367cad1f328
Kevin Sajan <kevsaj@gmail.com>
2020-11-16 10:37:31 -0500

    options list for Drinks please review

---
+++ assets/js/options.js
@@ -0,0 +1,235 @@
+options.js
+
+var Wine options =
+[
+  {
+    text: 'Light Alcohol Sweet White',
+    Calories: '111-147',
+    ABV: '6-9%'
+  },
+  {
+    text: 'High Alcohol Sweet White',
+    Calories: '177-213',
+    ABV: '9-12%'
+  },
+  {
+    text: 'Light Alchol Dry White',
+    Calories: '107-143',
+    ABV: '9-12%'
+  },
+  {
+    text: 'High Alcohol Dry White',
+    Calories: '153-173',
+    ABV: '12-14%'
+  },
+  {
+    text: 'Light Alchol Red',
+    Calories: '135-165',
+    ABV: '11-13.5%'
+  },
+  {
+    text: 'Sparkling Wine',
+    Calories: '158',
+    ABV: '12.5%'
+  },
+  {
+    text: 'Sweet Dessert Wine',
+    Calories: '189-275',
+    ABV: '14-21%'
+  }
+];
+
+var Pop options =
+[
+  {
+    text: 'Coke',
+    Calories: '149',
+    ABV: '0%'
+  },
+  {
+    text: 'Diet Coke',
+    Calories: '4',
+    ABV: '0%'
+  },
+  {
+    text: 'Dr. Pepper',
+    Calories: '96',
+    ABV: '0%'
+  },
+  {
+    text: 'Fanta',
+    Calories: '138',
+    ABV: '0%'
+  },
+  {
+    text: 'Pepsi',
+    Calories: '156',
+    ABV: '0%'
+  },
+  {
+    text: 'Sprite',
+    Calories: '131',
+    ABV: '0%'
+  },
+  {
+    text: 'Ginger Ale',
+    Calories: '124',
+    ABV: '0%'
+  }
+];
+
+var Cocktails options =
+[
+  {
+    text: 'Mojito',
+    Calories: '169',
+    ABV: '13%'
+  },
+  {
+    text: 'Sangria',
+    Calories: '175',
+    ABV: '13.9%'
+  },
+  {
+    text: 'Piña colada',
+    Calories: '242',
+    ABV: '~8%'
+  },
+  {
+    text: '*~Cosmopolitan~*',
+    Calories: '211',
+    ABV: '~20%'
+  },
+  {
+    text: 'Martini',
+    Calories: '215',
+    ABV: '~17%'
+  },
+  {
+    text: 'Manhattan',
+    Calories: '200',
+    ABV: '~30%'
+  },
+  {
+    text: 'Margarita',
+    Calories: '254',
+    ABV: '~8%'
+  }
+];
+
+var Liquor options =
+[
+  {
+    text: 'Champagne',
+    Calories: '96',
+    ABV: '12.5%'
+  },
+  {
+    text: 'Vodka',
+    Calories: '96',
+    ABV: '40%'
+  },
+  {
+    text: 'Gin',
+    Calories: '96',
+    ABV: '40%'
+  },
+  {
+    text: 'Rum',
+    Calories: '97',
+    ABV: '40%'
+  },
+  {
+    text: 'Whiskey',
+    Calories: '105',
+    ABV: '40%'
+  },
+  {
+    text: 'Scotch',
+    Calories: '105',
+    ABV: '40%'
+  },
+  {
+    text: 'Brandy',
+    Calories: '115',
+    ABV: '40%'
+  }
+];
+
+var Beer options =
+[
+  {
+    text: 'Bud Light',
+    Calories: '110',
+    ABV: '4.2%'
+  },
+  {
+    text: 'Corona Extra',
+    Calories: '148',
+    ABV: '4.6%'
+  },
+  {
+    text: 'Miller Lite',
+    Calories: '96',
+    ABV: '4.2%'
+  },
+  {
+    text: 'Modelo Especial',
+    Calories: '144',
+    ABV: '4.4%'
+  },
+  {
+    text: 'Heineken',
+    Calories: '142',
+    ABV: '5%'
+  },
+  {
+    text: 'Stella Artois',
+    Calories: '141',
+    ABV: '5%'
+  },
+  {
+    text: 'Dos Equis',
+    Calories: '131',
+    ABV: '4.7%'
+  }
+];
+
+var Smoothies options =
+[
+  {
+    text: 'Strawberry Banana Smoothie',
+    Calories: '650',
+    ABV: '0%'
+  },
+  {
+    text: 'Blueberry Blast Smoothie',
+    Calories: '465',
+    ABV: '0%'
+  },
+  {
+    text: 'Pineapple Coconut Smoothie',
+    Calories: '525',
+    ABV: '0%'
+  },
+  {
+    text: 'Penut Butter Oatmeal',
+    Calories: '730',
+    ABV: '0%'
+  },
+  {
+    text: 'Green Smoothie',
+    Calories: '515',
+    ABV: '0%'
+  },
+  {
+    text: 'Chocalate Banana Smoothie',
+    Calories: '805',
+    ABV: '0%'
+  },
+  {
+    text: 'Berry Smoothie',
+    Calories: '910',
+    ABV: '0%'
+  }
+];
